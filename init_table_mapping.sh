3770335f4da12e35bb39099b190005dc72df4557
xuxiangwen <eipi10@qq.com>
2019-01-16 12:32:45 +0800

    Update init_table_mapping.sh
    update task table mapping

--- init_table_mapping.sh
+++ init_table_mapping.sh
@@ -78,7 +78,7 @@ cat << EOF > $script_path/json/${task_id}_table.json
 }
 EOF
 
-task_id=cdc-A
+task_id=cdc-all
 cat << EOF > $script_path/json/${task_id}_table.json
 {
   "rules": [
@@ -91,28 +91,52 @@ cat << EOF > $script_path/json/${task_id}_table.json
         "table-name": "A"
       },
       "rule-action": "include"
-    }
-  ]
-}
-EOF
-
-task_id=cdc-B
-cat << EOF > $script_path/json/${task_id}_table.json
-{
-  "rules": [
+    },
     {
       "rule-type": "selection",
-      "rule-id": "1",
-      "rule-name": "1",
+      "rule-id": "2",
+      "rule-name": "2",
       "object-locator": {
         "schema-name": "schema1",
         "table-name": "B"
       },
       "rule-action": "include"
-    }
+    },
+  "rules": [
+    {
+      "rule-type": "selection",
+      "rule-id": "3",
+      "rule-name": "3",
+      "object-locator": {
+        "schema-name": "schema_1",
+        "table-name": "C"
+      },
+      "rule-action": "include"
+    },
+    {
+      "rule-type": "selection",
+      "rule-id": "4",
+      "rule-name": "4",
+      "object-locator": {
+        "schema-name": "schema_1",
+        "table-name": "D"
+      },
+      "rule-action": "include"
+    },
+    {
+      "rule-type": "selection",
+      "rule-id": "5",
+      "rule-name": "5",
+      "object-locator": {
+        "schema-name": "schema_1",
+        "table-name": "E"
+      },
+      "rule-action": "include"
+    }    
   ]
 }
 EOF
+
 
 
 ls -l  $script_path/json
